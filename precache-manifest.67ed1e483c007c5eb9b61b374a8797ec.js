924f193ef254594f318886560d31aed5a47d7be7
dan-sprucek <dan.sprucek@gmail.com>
2020-07-14 18:15:06 +0200

    Updates

--- precache-manifest.67ed1e483c007c5eb9b61b374a8797ec.js
+++ precache-manifest.67ed1e483c007c5eb9b61b374a8797ec.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62417421038dcc879e557de6ede45697",
+    "revision": "4698bb2b60a434fce5d4a1ee35ca3b55",
     "url": "/FCC-FrontEndCertificate-5-PomodoroClock/index.html"
   },
   {
-    "revision": "597bebc2cbfaedb43e3c",
+    "revision": "ed3627db354b015cd7b4",
     "url": "/FCC-FrontEndCertificate-5-PomodoroClock/static/css/main.180f4a32.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/FCC-FrontEndCertificate-5-PomodoroClock/static/js/2.83522744.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "597bebc2cbfaedb43e3c",
-    "url": "/FCC-FrontEndCertificate-5-PomodoroClock/static/js/main.19a8dab2.chunk.js"
+    "revision": "ed3627db354b015cd7b4",
+    "url": "/FCC-FrontEndCertificate-5-PomodoroClock/static/js/main.fc834a0f.chunk.js"
   },
   {
     "revision": "4344b864842fa5603670",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FCC-FrontEndCertificate-5-PomodoroClock/precache-manifest.cae40a4fba1d17e05d9ef3d34b783235.js"
+  "/FCC-FrontEndCertificate-5-PomodoroClock/precache-manifest.67ed1e483c007c5eb9b61b374a8797ec.js"
 );
 
 self.addEventListener('message', (event) => {
